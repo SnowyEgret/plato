581823b68dc948db18b53918fc2b4a9743d7e8d6
SnowyEgret <duncan.m.swain@gmail.com>
2014-06-05 14:45:27 -0400

    Tests for Matrices

--- src/main/java/ds/plato/geom/matrix/ReflectionMatrix.java
+++ src/main/java/ds/plato/geom/matrix/ReflectionMatrix.java
@@ -8,9 +8,11 @@ import ds.plato.geom.GeomUtil;
 
 public class ReflectionMatrix extends Matrix4d {
 
-	public ReflectionMatrix(Point3d p0, Point3d p1, Point3d p2) {
-		Matrix4d mT = new TranslationMatrix(new Vector3d(p0));
-		Matrix4d mR = new RotationMatrix(new Vector3d(0, 0, 1), GeomUtil.normalToPlane(p0, p1, p2));
+	public ReflectionMatrix(Point3d p0, Point3d p1, Point3d p2, Point3d p3) {
+		Vector3d v = new Vector3d();
+		v.sub(new Point3d(0, 0, 0), p0);
+		Matrix4d mT = new TranslationMatrix(v);
+		Matrix4d mR = new RotationMatrix(new Vector3d(0, 0, 1), GeomUtil.normalToPlane(p1, p2, p3));
 		setIdentity();
 		mul(mT);
 		mul(mR);
---
+++ src/main/java/ds/plato/geom/matrix/test/T_ScaleMatrix.java
@@ -0,0 +1,32 @@
+package ds.plato.geom.matrix.test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.closeTo;
+import static org.hamcrest.Matchers.equalTo;
+
+import javax.vecmath.Matrix4d;
+import javax.vecmath.Point3d;
+import javax.vecmath.Vector3d;
+
+import org.junit.Test;
+
+import ds.plato.geom.matrix.ScaleMatrix;
+import ds.plato.geom.test.GeomTest;
+
+public class T_ScaleMatrix extends GeomTest {
+
+	@Test
+	public void test() {
+		for (int i = 0; i < 100; i++) {
+			Vector3d v = vector();
+			Point3d p0 = p();
+			Point3d p1 = new Point3d(p0);
+			Matrix4d m = new ScaleMatrix(v);
+			m.transform(p1);
+			assertThat(p1.x, closeTo(p0.x * v.x, epsilon));
+			assertThat(p1.y, closeTo(p0.y * v.y, epsilon));
+			assertThat(p1.z, closeTo(p0.z * v.z, epsilon));
+		}
+	}
+
+}
